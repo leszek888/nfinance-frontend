f2cf06e6b2f28dd819aee6351a73534e2c65c9f5
Leszek Nowicki <nowickileszek88@gmail.com>
2021-07-13 12:09:35 +0200

    Add new files

---
+++ tests/unit/Filters.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+import Filters from '@/components/Filters.vue'
+
+describe('Filters.vue', () => {
+    it('renders correctly', async () => {
+        const wrapper = mount(Filters);
+    });
+});
+
+
