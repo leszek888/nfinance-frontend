b46a34f9666b6c801429d9b03690d38477f6cbf1
Leszek Nowicki <nowickileszek88@gmail.com>
2021-07-14 12:10:53 +0200

    FilterBox implemented

--- src/components/FilterBox.vue
+++ src/components/FilterBox.vue
@@ -1,8 +1,15 @@
 <template>
+    <div data-cy="container-filterbox">
+        <span class="filterbox-title">{{ title }}</span>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'FilterBox',
+
+    props: {
+        title: String,
+    },
 };
 </script>
--- tests/unit/FilterBox.spec.js
+++ tests/unit/FilterBox.spec.js
@@ -3,7 +3,12 @@ import FilterBox from '@/components/FilterBox.vue'
  
 describe('FilterBox.vue', () => {
     it('renders correctly', async() => {
-        const wrapper = mount(FilterBox);
+        const wrapper = mount(FilterBox, {
+            props: {
+                title: 'FilterTitle',
+            },
+        });
         expect(wrapper.find('[data-cy="container-filterbox"]').exists()).toBeTruthy();
+        expect(wrapper.find('[data-cy="container-filterbox"]').html()).toContain('FilterTitle');
     });
 });
