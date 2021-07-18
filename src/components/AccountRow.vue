64e4db06e831f7500b9d55c465ce80b2ebade874
Leszek Nowicki <nowickileszek88@gmail.com>
2021-07-18 16:45:24 +0200

    Added static + which will be used to collapse/show sub accounts in list

--- src/components/AccountRow.vue
+++ src/components/AccountRow.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="account_row">
-        {{ account.name }}, {{ account.balance }}<br />
+        <span>
+            <div v-if="subAccountsArePresent">&nbsp;+&nbsp;</div>
+            <div v-if="!subAccountsArePresent">&nbsp;&nbsp;&nbsp;</div>
+            {{ account.name }}
+        </span>
+        <span>{{ formattedBalance }}</span>
     </div>
     <div v-for="sub_account, index in account.sub_accounts" :key="index">
-        <div class="sub_arrow">
-            x
-        </div>
         <div class="sub_account">
             <account-row :account="sub_account" />
         </div>
@@ -13,8 +15,14 @@
 </template>
 
 <script>
+import { numberToString } from '@/helpers.js'
 export default {
     name: 'AccountRow',
+
+    computed: {
+        formattedBalance() { return numberToString(this.account.balance); },
+        subAccountsArePresent() { return this.account.sub_accounts.length > 0; },
+    },
 
     props: {
         account: { type: Object, required: true },
@@ -23,8 +31,14 @@ export default {
 </script>
 
 <style scoped>
+    span {
+        display: flex;
+    }
+
     .account_row {
         border-top: solid 1px #ccc;
+        display: flex;
+        justify-content: space-between;
         padding: 0.3em;
     }
     .sub_arrow {
