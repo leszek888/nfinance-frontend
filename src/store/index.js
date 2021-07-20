428752b5cd1bbe6f9b8f197cdf2412a9e0fa5b50
Leszek Nowicki <nowickileszek88@gmail.com>
2021-07-20 08:56:58 +0200

    Add .env files to handle hostname

--- src/store/index.js
+++ src/store/index.js
@@ -46,7 +46,7 @@ const store = createStore({
                 body = JSON.stringify(data);
             }
 
-            const response = await fetch('http://192.168.0.222:5000/api/'+url,
+            const response = await fetch(process.env.VUE_APP_ROOT_API+'/api/'+url,
                 {method: method, mode: 'cors', headers: headers, body: body})
 
             const json = await response.json();
@@ -77,7 +77,7 @@ const store = createStore({
         async getToken({ commit }) {
             let headers = new Headers();
             headers.append('Authorization', 'Basic ' + btoa(this.state.balanceId + ':pw'))
-            const response = await fetch('http://192.168.0.222:5000/api/auth', {method: 'GET', headers: headers, mode: 'cors'});
+            const response = await fetch(process.env.VUE_APP_ROOT_API+'/api/auth', {method: 'GET', headers: headers, mode: 'cors'});
             const data = await response.json();
             commit('setAuthToken', data['token']);
         },
