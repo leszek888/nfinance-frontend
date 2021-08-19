1c8e90d4c9ebc2ae806014b980cfa70cb9a607d1
Leszek Nowicki <nowickileszek88@gmail.com>
2021-08-19 19:24:29 +0200

    Update Tests

--- tests/unit/FilterDate.spec.js
+++ tests/unit/FilterDate.spec.js
@@ -1,8 +1,6 @@
 import { mount } from '@vue/test-utils'
 import FilterDate from '@/components/FilterDate.vue'
-import Filter from '@/components/Filter.vue'
 import FilterBox from '@/components/FilterBox.vue'
-import InputWithLabel from '@/components/InputWithLabel.vue'
  
 describe('FilterDate.vue', () => {
     it('renders correctly', async() => {
@@ -13,7 +11,6 @@ describe('FilterDate.vue', () => {
         });
 
         expect(wrapper.findComponent(FilterBox).exists()).toBeTruthy();
-        expect(wrapper.findComponent(FilterBox).findAllComponents(Filter)).toHaveLength(2);
     });
 
     it('updates dates after selecting period', async() => {
@@ -22,20 +19,20 @@ describe('FilterDate.vue', () => {
                 title: 'FilterDate',
             },
         });
-
+ 
         const dateStub = jest.spyOn(Date, 'now').mockImplementation(() => 1625517407660); // 2021-07-05
         wrapper.vm.$refs.periodselect.value = 'This Month';
         await wrapper.vm.updatePeriod();
-        expect(wrapper.find('[name="date_from"] input').element.value).toEqual('2021-07-01');
-        expect(wrapper.find('[name="date_to"] input').element.value).toEqual('2021-07-05');
+        expect(wrapper.vm.value_from).toEqual('2021-07-01');
+        expect(wrapper.vm.value_to).toEqual('2021-07-05');
         wrapper.vm.$refs.periodselect.value = 'Last Month';
         await wrapper.vm.updatePeriod();
-        expect(wrapper.find('[name="date_from"] input').element.value).toEqual('2021-06-01');
-        expect(wrapper.find('[name="date_to"] input').element.value).toEqual('2021-06-30');
+        expect(wrapper.vm.value_from).toEqual('2021-06-01');
+        expect(wrapper.vm.value_to).toEqual('2021-06-30');
         wrapper.vm.$refs.periodselect.value = 'This Year';
         await wrapper.vm.updatePeriod();
-        expect(wrapper.find('[name="date_from"] input').element.value).toEqual('2021-01-01');
-        expect(wrapper.find('[name="date_to"] input').element.value).toEqual('2021-07-05');
+        expect(wrapper.vm.value_from).toEqual('2021-01-01');
+        expect(wrapper.vm.value_to).toEqual('2021-07-05');
         dateStub.mockRestore();
     });
 
